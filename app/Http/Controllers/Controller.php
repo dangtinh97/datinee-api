ab49882a796ebe654dc4df9b9dce4cced20db5f5
Dang Tinh <>
2022-07-16 22:47:05 +0700

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use MongoDB\BSON\UTCDateTime;
 
 class Controller extends BaseController
 {
+    public function a(){
+        new UTCDateTime();
+    }
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
