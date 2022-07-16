dfa96618bc761ebdd0fad91d01c84a87bce763fe
Dang Tinh <>
2022-07-17 00:22:11 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login-with-facebook',[\App\Http\Controllers\Api\AuthController::class,'loginFacebook']);
+Route::get('login-with-facebook',[\App\Http\Controllers\Api\AuthController::class,'loginFacebook']);
